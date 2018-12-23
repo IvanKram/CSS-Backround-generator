2a06e4790fe0303000c02cba7d0360219e5c0e92
Ivan Kram <ivan.kramarenko1608@mail.ru>
2018-12-23 17:32:49 +0300

    Default CSS lines now show up on page load.

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ let css = document.querySelector("h3");
 let color1 = document.getElementById("color 1");
 let color2 = document.getElementById("color 2");
 let body =  document.getElementById("gradient");
+// making default CSS lines appear
+function setDefaultCSS(){
+    body.style.background = "linear-gradient(to right, " 
+    + color1.value 
+    + ", " 
+    + color2.value + ")"; 
+    
+    css.textContent = body.style.background + ";";
+}
+// calling the function which shows default CSS lines.
+setDefaultCSS();
 
 function setGradient() {
     body.style.background = "linear-gradient(to right, " 
