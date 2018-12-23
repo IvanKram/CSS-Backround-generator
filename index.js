ff47169a4dd4aeedb4a459bb0a0d1eec65177285
Ivan Kram <ivan.kramarenko1608@mail.ru>
2018-12-23 19:29:21 +0300

    Added random button to show a random gradient from two random colors generated on click.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let css = document.querySelector("h3");
 let color1 = document.getElementById("color 1");
 let color2 = document.getElementById("color 2");
 let body =  document.getElementById("gradient");
+let randomButton = document.getElementById("randomButton");
 // making default CSS lines appear
 function setDefaultCSS(){
     body.style.background = "linear-gradient(to right, " 
@@ -13,7 +14,7 @@ function setDefaultCSS(){
 }
 // calling the function which shows default CSS lines.
 setDefaultCSS();
-
+// Getting a gradient created from the two inputs.
 function setGradient() {
     body.style.background = "linear-gradient(to right, " 
     + color1.value 
@@ -23,5 +24,24 @@ function setGradient() {
     css.textContent = body.style.background + ";";
 }
 
+function get_random_color() {
+    function c() {
+      var hex = Math.floor(Math.random()*256).toString(16);
+      return ("0"+String(hex)).substr(-2); // pad with zero
+    }
+    return "#"+c()+c()+c();
+  }
+  
+  
+  
+  function setRandomColor() {
+    //$("#colorpad").css("background-color", getRandomColor());
+    color1.value = get_random_color();
+    color2.value = get_random_color();
+    setGradient();
+  }
+  
+
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+randomButton.addEventListener("click", setRandomColor);
