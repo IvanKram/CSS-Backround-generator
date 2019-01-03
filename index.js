6bc3836842c4e84bb40e9fdfd47967798efdc8c3
Ivan Kram <ivan.kramarenko1608@mail.ru>
2019-01-03 16:00:57 +0300

    Added the possibility of a 3rd color being used.

--- index.js
+++ index.js
@@ -3,31 +3,64 @@ let color1 = document.getElementById("color 1");
 let color2 = document.getElementById("color 2");
 let body =  document.getElementById("gradient");
 let randomButton = document.getElementById("randomButton");
+let colorBox = document.getElementById("colorBox");
 let directionSel = document.getElementById("directionSel");
+let colorSel = document.getElementById("colorSel");
+let color3;
+let colorSwCounter = 0;
 // making default CSS lines appear
 function setDefaultCSS(){
 	body.style.background = "linear-gradient(to right, "
 		+ color1.value
 		+ ", "
-		+ color2.value + ")";
-
+		+ color2.value
+		+ ")";
 	css.textContent = body.style.background + ";";
 }
 // calling the function which shows default CSS lines.
 setDefaultCSS();
 
+// adding a 3rd color option
+function add3color() {
+	if (colorSel.value === "3" && colorSwCounter === 0) {
+		let input3 = document.createElement("INPUT");
+		input3.setAttribute("type", "color");
+		input3.setAttribute("name", "color3");
+		input3.setAttribute("id", "color 3");
+		input3.setAttribute("value", "#0000ff");
+		colorBox.appendChild(input3);
+		color3 = document.getElementById("color 3");
+		setGradient();
+		colorSwCounter++;
+
+	} else {
+		colorBox.removeChild(color3);
+	}
+}
 //Getting a gradient created from the two inputs.
 function setGradient() {
-	body.style.background = directionSel.value
-		+ color1.value
-		+ ", "
-		+ color2.value + ")";
-
+	if (color3 === undefined) {
+		body.style.background = directionSel.value
+			+ color1.value
+			+ ", "
+			+ color2.value
+			+ ")";
+	} else {
+		body.style.background = directionSel.value
+			+ color1.value
+			+ ", "
+			+ color2.value
+			+ ", "
+			+ color3.value
+			+ ")";
+	}
 	css.textContent = body.style.background + ";";
 }
 
+// getting a gradient created for 3 inputs
 
 
+// color settings
 function get_random_color() {
 	function c() {
 		var hex = Math.floor(Math.random()*256).toString(16);
@@ -40,16 +73,21 @@ function get_random_color() {
 
 function setRandomColor() {
 	//$("#colorpad").css("background-color", getRandomColor());
-	color1.value = get_random_color();
-	color2.value = get_random_color();
+	if (color3 === undefined) {
+		color1.value = get_random_color();
+		color2.value = get_random_color();
+	} else {
+		color1.value = get_random_color();
+		color2.value = get_random_color();
+		color3.value = get_random_color();
+	}
 	setGradient();
 }
 
 
-
-
-
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+//color1.addEventListener("input", setGradient);
+//color2.addEventListener("input", setGradient);
 randomButton.addEventListener("click", setRandomColor);
 directionSel.addEventListener("input", setGradient);
+colorSel.addEventListener("input", add3color);
+colorBox.addEventListener("input", setGradient);
